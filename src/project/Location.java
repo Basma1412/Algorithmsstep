d8485b4e0c3d991293ad6874932fa8b1c62955a7
Basma1412 <13p6036@eng.asu.edu.eg>
2017-01-02 23:35:57 +0200

    second commit

---
+++ src/project/Location.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project;
+
+class Location {
+
+    double x;
+    double y;
+
+    public Location(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+}
--- src/project/Project.java
+++ src/project/Project.java
@@ -7,6 +7,8 @@ public class Project {
 
     public static void main(String[] args) {
 
+        
+        WorkingArea.testExcute();
     }
     
 }
