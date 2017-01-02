6cb88b279493323949a0eddb73dd88a49bcadd78
Basma1412 <13p6036@eng.asu.edu.eg>
2017-01-03 00:34:20 +0200

    Find shortest path

--- src/project/WorkingArea.java
+++ src/project/WorkingArea.java
@@ -12,45 +12,48 @@ public class WorkingArea {
     static List<Edge> edges;
 
     public static void testExcute() {
-        nodes = new ArrayList<Vertex>();
-        edges = new ArrayList<Edge>();
+        nodes = new ArrayList<>();
+        edges = new ArrayList<>();
 
-        for (int i = 0; i < 11; i++) {
+        for (int i = 0; i < 6; i++) {
             Vertex location = new Vertex("Node_" + i, "Node_" + i);
             nodes.add(location);
         }
-Vertex a=nodes.get(2);
-Vertex b=nodes.get(6);
+        Vertex a = nodes.get(2);
+        Vertex b = nodes.get(5);
+        a.setLocation(1, 5);
+        b.setLocation(200, 7);
 
-a.setLocation(1, 5);
-b.setLocation(200,7);
-       makeEdges("Edge_0", a, b);
-//        addLane("Edge_1", 0, 2);
-//        addLane("Edge_2", 0, 4);
-//        addLane("Edge_3", 2, 6);
-//        addLane("Edge_4", 2, 7);
-//        addLane("Edge_5", 3, 7);
-//        addLane("Edge_6", 5, 8);
-//        addLane("Edge_7", 8, 9);
-//        addLane("Edge_8", 7, 9);
-//        addLane("Edge_9", 4, 9);
-//        addLane("Edge_10", 9, 10);
-//        addLane("Edge_11", 1, 10);
+        Vertex d = nodes.get(3);
+        Vertex e = nodes.get(4);
+        d.setLocation(2, 51);
+        e.setLocation(2, 31);
 
-        // Lets check from location Loc_1 to Loc_10
+        Vertex f = nodes.get(1);
+        Vertex g = nodes.get(0);
+        f.setLocation(1, 30);
+        g.setLocation(2, 30);
+
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                if (i == j) {
+                    continue;
+                }
+                makeEdges("Edge" + (i + j), nodes.get(i), nodes.get(j));
+            }
+        }
         Graph graph = new Graph(nodes, edges);
         BestRoute dijkstra = new BestRoute(graph);
-        dijkstra.execute(nodes.get(2));
-        LinkedList<Vertex> path = dijkstra.getPath(nodes.get(6));
+        dijkstra.execute(nodes.get(0));
+        LinkedList<Vertex> path = dijkstra.getPath(nodes.get(3));
 
-        if (path==null)
-        {
-            
+        if (path == null) {
+
+        } else {
+            for (int i = 0; i < path.size(); i++) {
+                System.out.println(path.get(i));
+            }
         }
-        else
-        {for (int i=0;i<path.size();i++) {
-            System.out.println(path.get(i));
-        }}
 
     }
 
@@ -58,14 +61,11 @@ b.setLocation(200,7);
         int distance = (int) a.distance(b);
         if (distance <= 20) {
             Edge lane = new Edge(laneId, a, b, distance);
-                  edges.add(lane);
+            edges.add(lane);
 
         }
     }
 
-    public static void addLane(String laneId, int sourceLocNo, int destLocNo) {
-        Edge lane = new Edge(laneId, nodes.get(sourceLocNo), nodes.get(destLocNo));
-        edges.add(lane);
-    }
+
 
 }
